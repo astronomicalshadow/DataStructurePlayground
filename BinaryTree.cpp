6775813e8cdc671f5ebdf0a2ef1574871b682fc9
kuang <snowy-shadow@github.com>
2023-02-19 21:23:40 -0600

    bare bone binary tree insertion completed - still no traveral

--- BinaryTree.cpp
+++ BinaryTree.cpp
@@ -2,51 +2,72 @@
 #include <vector>
 
 template<class T>
-class BinaryTree
+class BinarySearchTree
 {
-    enum TreeType
+public:
+    BinarySearchTree(const std::vector<T> &Data)
     {
-        BST,
-        BBT
-    };
+        m_Head.data = Data[0];
+        // begin()++ doesn't work
+        std::vector<T> Vector(Data.begin() + 1, Data.end());
+        Insert(Vector);
+    }
 
+    void Insert(const std::vector<T> &Data)
+    {
+        m_Cursor = &m_Head;
+        // go through the list
+        for (auto &Iter : Data)
+        {
+            if (Iter <= m_Cursor->data)
+            {
+                m_Cursor->LeftNode = new m_Node();
+                m_Cursor = m_Cursor->LeftNode;
+                m_Cursor->data = Iter;
+                m_Height++;
+                m_Width++;
+            }
+            else
+            {
+                m_Cursor->RightNode = new m_Node();
+                m_Cursor = m_Cursor->RightNode;
+                m_Cursor->data = Iter;
+                m_Height++;
+                m_Width++;
+            }
+
+        }
+    }
+
+    void Print()
+    {
+        // only prints left node and the last element doesn't get printed
+        m_Cursor = &m_Head;
+        while(m_Cursor->LeftNode != nullptr)
+        {
+            std::cout << m_Cursor->data;
+            m_Cursor = m_Cursor->LeftNode;
+        }
+    }
+
+private:
     struct m_Node
     {
         T data;
-        struct m_Node* LeftNode,* RightNode = nullptr;
+        struct m_Node *LeftNode, *RightNode = nullptr;
         int NodeDegree = 0;
     };
 
-    struct m_Node* m_Head = nullptr;
-    int m_TreeSize;
-
-public:
-
-    void CreateTree(std::vector<T> items)
-    {
-        if (m_TreeSize > 0)
-        {
-            void Insert(items);
-            return;
-        }
-        
-        m_Head = new m_Node;
-        m_Head->data = items[0];
-        if (items.size() > 1)
-        {
-            Insert(items);
-        }
-
-    }
-
-    void Insert(std::vector<T> items, TreeType TreeType)
-    {}
+    m_Node m_Head{};
+    m_Node *m_Cursor = nullptr;
+    int m_Height, m_Width = 0;
 };
 
 int main()
 {
-    BinaryTree<int> tree;
+    BinarySearchTree<int> Tree({6, 4, 2, 1, 0});
+    Tree.Print();
 
-    std::cout << "Hello World";
+
 
 }
--- README.md
+++ README.md
@@ -5,3 +5,4 @@
 \+ Prime Number - finds the nth prime number<br>
 \+ State Machine with a singleton design <br>
 \+ Convert 32 bit to 16 bit floating point IEEE 754
+\+ Bare bone binary tree implementation <br>
